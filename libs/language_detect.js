30dcc58c00a08268aeea90b4b0428f348f03f9fd
Tar <chayopart@gmail.com>
2021-05-22 23:06:46 +0700

    25

---
+++ libs/language_detect.js
@@ -0,0 +1,21 @@
+var userLang = window.navigator.language || window.navigator.userLanguage; 
+// thai language
+// alert(userLang);
+if (userLang == "th-TH") {
+	window.location.href = "index_th.htm";
+}
+else if (userLang == "th") {
+	window.location.href = "index_th.htm";
+}
+// english language
+else if(userLange == "en-US") {
+	window.location.href = "index_en.htm";
+}
+else if(userLange == "en") {
+	window.location.href = "index_en.htm";
+}
+// other language
+else {
+	window.location.href = "index_en.htm";
+}
+// alert ("The language is: " + userLang);
